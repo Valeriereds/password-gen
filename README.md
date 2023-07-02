5a8a4491203173852fc40440908272031727d6b0
Valeriereds <valeriereds@gmail.com>
2023-07-01 22:34:51 -0500

    Assigned vars to functions, and added a random num gen

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # password-gen
+
+## Description
+
+I've created this random password generator to showcase all the skills I have learned so far using Javascript. 
--- script.js
+++ script.js
@@ -23,63 +23,92 @@ function generatePassword() {
   var howMany = window.prompt("How many characters do you want in your password?");
   console.log(howMany);
   // prompt for how many characters
+  if (!userCharacters) {
+    return;
+  }
   
+  if (isNaN(userCharacters)) {
+    window.alert("You must enter a valid number!");
+    return;
+  }
+
+  if (userCharacters < 8 || userCharacters > 128) {
+    window.alert("Your password must be at least 8 characters long and no more than 128.");
+    return;
+  };
+
   if (howMany < 8 || howMany > 128) {
     window.alert("Please choose a length between 8-128 characters."); {
       return;
     }
   } 
+  // need a prompt to stop anything but numbers for the length
+  // isNan to stop anything other than numbers not a number
+  if (isNaN(howMany)) {
+    window.alert("Please input a number.");
+    return;
+  }
+  // add a variable in your confirm statement
+  // window.confirM
+ 
   var doUpper = window.confirm("Do you want UPPER CASE letters in your password?");
   var doLower = window.confirm("Do you want lower case letters in your password?");
   var doSpec = window.confirm("Do you want Special Characters in your password?");
   var doNumb = window.confirm("Do you want numbers in your password?");
-  
-  
-  
   // // run the function
+  // var for char
+  // what type of char
+  // special, number, upper, lower
+  // example var upperLetters = ["A", "B", "C"]
+  // vars that includes all possible char
+  // separate vars for each type, arrays
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "{", "}", "|", "~"];
-  console.log(upperCase, lowerCase, numbers, specChar);
+  // console.log(upperCase, lowerCase, numbers, specChar);
+  // use answers to make password
+  // take all true arrays and concat into new array
+  // choose random chars from arr based on user input
+   // take all true arrays and concat into new array
 
-  if (specChar && numbers && upperCase && lowerCase) {
-    var allConfirm = specialChar.concat(numbers, upperCase, lowerCase);
-    } else if (specialChar && numbers && upperCase) {
-      var spNumbUp = specialChar.concat(numbers, upperCase);
-    } else if (specialChar && numbers) {
-      var specNumb = specialChar.concat(numbers);
-    } else if (specNum && lowerCase) {
-      var spNumLower = specialChar.concat(number, lowerCase); }
-      else if (lowerCase && upperCase) {
-      var lowerUp = (lowerCase.concat(upperCase))
-    }
-  console.log (allTrue)
-}
+  var newPass = [];
   
+  if (doSpec) {   
+    newPass = specChar.concat(newPass);
+  }; 
+  if (doNumb) {
+    newPass = numbers.concat(newPass);
+  };
+  if (doUpper) {
+    newPass = upperCase.concat(newPass);
+  };
+  if (doLower) {
+    newPass = lowerCase.concat(newPass);
+  };
+  if (!doSpec && !doNumb && !doUpper && !doLower) {
+    window.alert("You must choose at least one type of character to create a password. Please try again.");
+      return;
+  };
+  // console.log(newPass) testing, testing
+}
+// var push math.random into new array
+// need a for loop to choose a random number
+  var newNum = []
+  
+  for (var i = 0; i < howMany; i++) {
+    var rdmNum = newPass[Math.floor(Math.random() * newPass.length)];
+    newNum.push(rdmNum)
+  
+   // var password = turn array into string .toString()
+
+  var password = newNum.join("").toString();
+  
+  return password;
+
+  }
+
 generatePassword()
   
 
-  
-  
-
-
-
-  // prompt how many characters
-  // var for char
-  
-  // what type of char
-  // special, number, upper, lower
-  // window.confirm
-
-  // use answers to make password
-  // vars that includes all possible char
-  // separate vars for each type, arrays
-  // example var upperLetters = ["A", "B", "C"]
-
-  // take all true arrays and concat into new array
-  // choose random chars from arr based on user input
-  // var push into new array math.random into new array
-  // var password = turn array into string .toString()
-
-  // return password;
+ // return password;
